1418dfe648a74d53fe02f55c1820502228e5b189
6Thienthien6 <thien502760@gmail.com>
2022-08-23 17:06:39 +0700

    cover

--- Bt Student.sql
+++ Bt Student.sql
@@ -31,6 +31,31 @@ CREATE TABLE Mark
     Mark      FLOAT   DEFAULT 0 CHECK ( Mark BETWEEN 0 AND 100),
     ExamTimes TINYINT DEFAULT 1,
     UNIQUE (SubId, StudentId),
-    FOREIGN KEY (SubId) REFERENCES Subject (SubId),
+    FOREIGN KEY (SubId) REFERENCES Subjects (SubId),
     FOREIGN KEY (StudentId) REFERENCES Student (StudentId)
 );
+-- thêm thông tin vào bảng class.
+-- INSERT INTO `quanlysinhvien`.`class` (`ClassId`, `ClassName`, `StartDate`, `Statuss`) VALUES ('2', 'AMT2 ', '2022-08-23', b'0');
+-- thêm thông tin vào bảng student.
+insert into `quanlysinhvien`.`student` ( `StudentName`, `Address`, `Phone`, `Statuss`, `ClassId`) values ('Khanh', 'HCM', '02938274923', b'0', b'1');
+-- INSERT INTO `quanlysinhvien`.`student` (`StudentName`, `Address`, `Phone`, `Statuss`, `ClassId`) VALUES ('Hanh', 'HCM', '029392393', b'0', b'1');
+insert into quanlysinhvien.student ( StudentName, Address, Phone, Statuss, ClassId) values('Helen', 'Ha Noi', '02959473824', b'1', b'1');
+-- xóa thông tin hàng id thứ 3 của bảng student
+UPDATE `quanlysinhvien`.`student` SET `StudentId` = '', `StudentName` = NULL, `Address` = NULL, `Phone` = NULL, `Statuss` = NULL, `ClassId` = NULL WHERE (`StudentId` = '3');
+-- đẩy vị trí thông tin hàng thứ 4 lên nối tiếp số thứ tự trong danh sách ở bảng student 
+UPDATE `quanlysinhvien`.`student` SET `StudentId` = '3' WHERE (`StudentId` = '4');
+insert into Subjects values
+(1, 'CF', 4 , 1),
+(2, 'LM', 3, 1),
+(3, 'TS', 6, 1),
+(4, 'AT', 2, 1);
+insert into Mark (SubId, StudentId, Mark, ExamTimes)
+values
+(1, 3, 8, 2),
+(1, 2, 7, 1),
+(1, 1, 6, 1);
+
+
+
+
+
